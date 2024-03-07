8b56e155c9006bc75e792bb980f2c1d288585c6d
spencerengmann <spencerengmann@gmail.com>
2024-03-06 17:36:54 -0700

    Did the Prep 2 assisgnment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+        job1._companyName = "Microsoft Corporation";
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Systems Administrator";
+        job2._startYear = 2017;
+        job2._endYear = 2018;
+        job2._companyName = "Apple";
+        job2.Display();
+
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Ace Snypa";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
     }
 }
